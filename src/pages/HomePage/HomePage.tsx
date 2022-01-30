d34472ae5f0626f1c966168161ad309344126138
liadaltiff <liadaltif@gmail.com>
2022-01-30 11:39:02 +0200

    set the react router, and created a main page, which is probably going the be the only page in this project

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,11 @@
+import classes from "./home-page.module.scss";
+
+const HomePage: React.FC = () => {
+  return (
+    <div className={classes.root}>
+      <h1>THIS IS A TEXT</h1>
+    </div>
+  );
+};
+
+export default HomePage;
