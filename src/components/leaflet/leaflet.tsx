d8f86af52c63c614537990a420b9d6dc9559d9ba
liadaltiff <liadaltif@gmail.com>
2022-01-30 17:17:12 +0200

    added borders to tel aviv city

--- src/components/leaflet/leaflet.tsx
+++ src/components/leaflet/leaflet.tsx
@@ -1,12 +1,32 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  Polyline,
+  Polygon,
+} from "react-leaflet";
 import classes from "./leaflet.module.scss";
-import neighbourhoods from "../../../neighbourhoods.json";
-import useSwr from "swr";
+import data from "../../../neighbourhoods.json";
+import { LatLngExpression } from "leaflet";
+import { useMemo } from "react";
 
 const Leaflet = () => {
+  const purpleOptions = { color: "white", fillColor: "#e76f51" };
+  const multyLine = useMemo(
+    () =>
+      data.features.map((neighbourhood) => {
+        return neighbourhood.geometry.coordinates[0].map((cordinates) => {
+          return [cordinates[1], cordinates[0]];
+        });
+      }) as LatLngExpression[][],
+    []
+    //update
+  );
+
   return (
     <MapContainer
-      center={[32.0144, 34.79635]}
+      center={[32.08, 34.778]}
       zoom={13}
       scrollWheelZoom={true}
       className={classes.mapContainer}
@@ -15,11 +35,20 @@ const Leaflet = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[32.0144, 34.79635]}>
-        <Popup>
-          A Popup <br /> Easily customizable.
-        </Popup>
-      </Marker>
+
+      <Polygon pathOptions={purpleOptions} positions={multyLine}></Polygon>
+      {data.features.map((neighbourhood) => {
+        return (
+          <Polyline
+            pathOptions={purpleOptions}
+            positions={
+              neighbourhood.geometry.coordinates[0].map((cordinates) => {
+                return [cordinates[1], cordinates[0]];
+              }) as LatLngExpression[]
+            }
+          />
+        );
+      })}
     </MapContainer>
   );
 };
