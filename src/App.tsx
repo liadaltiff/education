b2a0b4571d0bf9841ab828f958ace7e16dcf80b1
liadaltiff <liadaltif@gmail.com>
2022-01-30 11:33:00 +0200

    changed the default files I got from opening a new vite app

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,11 @@
-import { useState } from "react";
-import logo from "./logo.svg";
+import { useContext, useEffect, useState } from "react";
 import "./App.css";
+import { Routes, Route, Navigate } from "react-router";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p></p>
-
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <Routes>
+      <Route path="*" element={<Navigate to="/home" />} />
+    </Routes>
   );
 }
-
-export default App;
