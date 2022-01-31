f11c1a2c9575c06d8bca92006217a512d1195903
liadaltiff <liadaltif@gmail.com>
2022-01-31 14:09:48 +0200

    added the sidebar component with functionality

---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,55 @@
+import React, { useContext, useEffect, useState } from "react";
+import Institute from "../Institute/Institute";
+import Institutes from "../Institutes/Institutes";
+import Neighbourhoods from "../neighbourhoods/neighbourhoods";
+import classes from "./sidebar.module.scss";
+import clsx from "clsx";
+
+function SideBar() {
+  const [activeTab, setActiveTab] = useState("שכונות");
+
+  return (
+    <div>
+      <div className={classes.container}>
+        <div className={classes.selectionAreaText}>
+          <h5>בחר אזור</h5>
+        </div>
+        <div className={classes.selectionArea}>
+          <div className={classes.selectionAreaNavBar}>
+            <button
+              className={clsx(
+                classes.unActiveBtn,
+                activeTab === "שכונות" && classes.activeBtn
+              )}
+              onClick={() => setActiveTab("שכונות")}
+            >
+              שכונות
+            </button>
+            <button
+              className={clsx(
+                classes.unActiveBtn,
+                activeTab === "מוסדות" && classes.activeBtn
+              )}
+              onClick={() => setActiveTab("מוסדות")}
+            >
+              מוסדות
+            </button>
+            <button
+              className={clsx(
+                classes.unActiveBtn,
+                activeTab === "מוסד" && classes.activeBtn
+              )}
+              onClick={() => setActiveTab("מוסד")}
+            >
+              מוסד
+            </button>
+          </div>
+          {activeTab === "שכונות" && <Neighbourhoods />}
+          {activeTab === "מוסדות" && <Institutes />}
+          {activeTab === "מוסד" && <Institute />}
+        </div>
+      </div>
+    </div>
+  );
+}
+export default SideBar;
