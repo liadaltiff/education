fdd9f167a52cb1ceab2032707f2442a5e0b9715b
liadaltiff <liadaltif@gmail.com>
2022-01-31 19:04:54 +0200

    another update

--- src/components/neighbourhoods/neighbourhoods.tsx
+++ src/components/neighbourhoods/neighbourhoods.tsx
@@ -1,9 +1,23 @@
 import React, { useContext, useState } from "react";
 import classes from "./neighbourhoods.module.scss";
 import data from "../../../neighbourhoods.json";
+import { SelectedNeighbourhoodIdContext } from "../../contexts/neighbourhoodContext";
+import { ActiveTabContext } from "../../contexts/activeTabContext";
 
 function Neighbourhoods() {
   const [neighbourhoodSearch, setNeighbourhoodSearch] = useState("");
+  const { activeTab, setActiveTab } = useContext(ActiveTabContext);
+  const {
+    selectedNeighbourhoodId,
+    setSelectedNeighbourhoodId,
+    setSelectedNeighbourhoodName,
+  } = useContext(SelectedNeighbourhoodIdContext);
+
+  const setContexts = (neighbourhoodId: string, neighbourhoodName: string) => {
+    setActiveTab("מוסדות");
+    setSelectedNeighbourhoodId(neighbourhoodId);
+    setSelectedNeighbourhoodName(neighbourhoodName);
+  };
 
   return (
     <div className={classes.container}>
@@ -20,7 +34,16 @@ function Neighbourhoods() {
         {data.features.map((neighbourhood) => {
           if (neighbourhood.properties.shemshchun.includes(neighbourhoodSearch))
             return (
-              <div className={classes.neighbourhood}>
+              <div
+                key={neighbourhood.properties.UniqueId}
+                className={classes.neighbourhood}
+                onClick={() =>
+                  setContexts(
+                    neighbourhood.properties.UniqueId,
+                    neighbourhood.properties.shemshchun
+                  )
+                }
+              >
                 <a>{neighbourhood.properties.shemshchun}</a>
               </div>
             );
