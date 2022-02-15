a1fcaeb37d63af599e5120b032cceee7ec57f8e9
liadaltiff <liadaltif@gmail.com>
2022-02-15 11:27:38 +0200

    fixed transfer amount component styling and functionality

--- src/components/sidebar/navigation/institute/instituteTransfer/InstituteTransfer.tsx
+++ src/components/sidebar/navigation/institute/instituteTransfer/InstituteTransfer.tsx
@@ -4,111 +4,89 @@ import classes from "./institute-transfer.module.scss";
 import { distance } from "./utils";
 import data from "../../../../../../neighbourhoods.json";
 import { Institute } from "../../../../../types/institute.type";
+import { TextField } from "@mui/material";
+import { DataContext } from "../../../../../contexts/DataContext";
 
-interface SelectedInstituteProps {
-  institute: Institute;
-}
-
-const InstituteTransfer: FC<SelectedInstituteProps> = ({ institute }) => {
-  const [selected, setSelcted] = useState<Institute>();
+function InstituteTransfer() {
   const { selectedInstitute } = useContext(SelectedInstituteContext);
-  const [selectedInstituteState, setSelectedInstituteState] =
-    useState<Number>();
-  const [amountToTransfer, setAmountToTransfer] = useState(0);
+  const { hoods } = useContext(DataContext);
 
-  const findSelectedInstitute = (selectedId: number) => {
-    const schools = data.features.map(
-      (neighbourhood) => neighbourhood.properties.schools
-    );
-    for (let i = 0; i < schools.length; i++) {
-      const selectedInst = schools[i].find(({ id }) => id === selectedId);
-      if (selectedInst) {
-        setSelcted(selectedInst);
-        console.log(selectedInst);
-        return;
-      }
-    }
-  };
-
-  const transferStudents = () => {
-    const toLog = {
-      from: selectedInstitute?.name,
-      typeOfSender: selectedInstitute?.type,
-      to: selected?.name,
-      typeOfGetter: selected?.type,
-      amount: amountToTransfer,
-    };
-
-    console.log("selected", JSON.stringify(toLog, null, 2));
-  };
+  const [amountToTransfer, setAmountToTransfer] = useState<number>(0);
+  const [choosenSchool, setChoosenSchool] =
+    useState<[number, string, number]>();
 
   return (
     <div className={classes.container}>
-      <div className={classes.sendHomeHeader}>
-        <p className={classes.sendHomeHeaderText}>העבר אל</p>
+      <div className={classes.moveToHeader}>
+        <p className={classes.moveToHeaderText}>העבר אל</p>
         <div className={classes.sendHomeContainer}>
-          <div className={classes.sendHome}>שלח הביתה</div>
+          {selectedInstitute && (
+            <div
+              className={classes.sendHome}
+              onClick={() => {
+                // saveMovment(
+                //   "SENDHOME",
+                //   selectedInstitute.id,
+                //   selectedInstitute.total_students
+                // );
+              }}
+            >
+              שלח הביתה
+            </div>
+          )}
         </div>
       </div>
       <div className={classes.instituteContainer}>
         {selectedInstitute &&
+          hoods &&
           distance(selectedInstitute).map((school: any) => {
-            const [isClicked, setIsClicked] = useState(false);
-
             return (
-              <>
+              <div className={classes.schoolContainer}>
                 <div
-                  onClick={() => {
-                    console.log("school.id", school[0]);
-                    findSelectedInstitute(school[0]);
-                    setIsClicked(!isClicked);
-                    setSelectedInstituteState(school.id);
-                  }}
                   key={school[0]}
                   className={classes.school}
+                  onClick={() => setChoosenSchool(school)}
                 >
                   <a className={classes.distance}>{school[2]} ק"מ</a>
                   <a>{school[1]}</a>
                 </div>
-
-                <div>
-                  {isClicked &&
-                    Menu(transferStudents, {
-                      get: amountToTransfer,
-                      set: setAmountToTransfer,
-                    })}
-                </div>
-              </>
+                {choosenSchool && choosenSchool[0] === school[0] && (
+                  <div className={classes.moveToContainer}>
+                    <div
+                      className={classes.send}
+                      onClick={() => {
+                        // saveMovment(
+                        //   "TRANSFER",
+                        //   selectedInstitute.id,
+                        //   amountToTransfer,
+                        //   choosenSchool[0]
+                        // );
+                      }}
+                    >
+                      העבר
+                    </div>
+                    <input
+                      // sx={{ width: "15ch" }}
+                      className={classes.moveToInput}
+                      id="filled-number"
+                      // label="כמות להעברה"
+                      type="number"
+                      // InputLabelProps={{
+                      //   shrink: true,
+                      // }}
+                      // variant="filled"
+                      placeholder="כמות להעברה"
+                      onChange={(event) => {
+                        setAmountToTransfer(Number(event.target.value));
+                      }}
+                    />
+                  </div>
+                )}
+              </div>
             );
           })}
       </div>
     </div>
   );
-};
+}
 export default InstituteTransfer;
-
-const Menu = (
-  transferFn: () => void,
-  amount: {
-    get: number;
-    set: (value: number) => void;
-  }
-) => {
-  return (
-    <div className={classes.openedPanel}>
-      <button onClick={transferFn} className={classes.transferButton}>
-        העבר
-      </button>
-      <input
-        type="number"
-        id="amountToTransferx"
-        className={classes.openedPanelText}
-        placeholder="כמות להעביר"
-        onChange={(e) => {
-          amount.set(parseInt(e.currentTarget.value ?? 0));
-        }}
-        value={amount.get}
-      ></input>
-    </div>
-  );
-};
